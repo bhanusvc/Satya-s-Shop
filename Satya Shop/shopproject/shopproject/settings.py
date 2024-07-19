bed4d26523aa8ab99d0f070eb281dcaaee54bb7a
Bhanu Nimmala <nimmalabhanusvc@gmail.com>
2024-07-19 11:56:47 +0530

    added customerapp in settings.py in installed apps.

--- Satya Shop/shopproject/shopproject/settings.py
+++ Satya Shop/shopproject/shopproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', # for new apps add it here like 'demoapp'
+    'customerapp',
 ]
 
 MIDDLEWARE = [
