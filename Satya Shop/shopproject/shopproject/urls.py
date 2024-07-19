f92449bcf9e0255465413dcab86a38df5d5b4d4a
Bhanu Nimmala <nimmalabhanusvc@gmail.com>
2024-07-19 12:41:30 +0530

    included clientapp urls in main project urls

--- Satya Shop/shopproject/shopproject/urls.py
+++ Satya Shop/shopproject/shopproject/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("customerapp.urls")),
     path("", views.landingpage, name="landing"),
     path("home", views.homepage, name="home"),
     path("login", views.loginpage, name="login"),
