d365b5209df7c454f0f9ae2498844aa3769c6e5f
Bhanu Nimmala <nimmalabhanusvc@gmail.com>
2024-07-21 10:10:09 +0530

    changes to CSS and added ph no validation in forms.py

--- Satya Shop/shopproject/customerapp/forms.py
+++ Satya Shop/shopproject/customerapp/forms.py
@@ -7,4 +7,13 @@ class RegistrationForm(forms.ModelForm):
         fields = ['fullname', 'gender', 'email', 'username', 'password', 'contact']
         widgets = {
             'password': forms.PasswordInput(),
+            'contact': forms.TextInput(attrs={'type': 'tel'}),
         }
+
+    contact = forms.RegexField(
+        regex=r'^\+?1?\d{10}$',  # Adjust the regex according to your requirement
+        error_messages={
+            'invalid': 'Enter a valid contact number. Up to 10 digits allowed.'
+        }
+    )
+
